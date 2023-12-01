0f5596f91636c6e80bc9d2a2fe9e66d5b5774d51
valen-tino <valentinojehaut@gmail.com>
2023-12-01 09:36:34 +0800

    Usecase 1 & 2 UI Already Done

--- src/app/admin/merchant-approval/merchant-approval.component.ts
+++ src/app/admin/merchant-approval/merchant-approval.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./merchant-approval.component.css']
 })
 export class MerchantApprovalComponent {
+  merchants: any[] = []; // Change the type based on your merchant data structure
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    // Use HttpClient to load data from the JSON file
+    this.http.get<any[]>('assets/datas/merchants.json').subscribe(
+      (data) => {
+        this.merchants = data;
+      },
+      (error) => {
+        console.error('Error loading merchant data:', error);
+      }
+    );
+  }
 }
